e64d985fdcebac754d96c2b2de7c8f511c8f53af
Ashley Pratt <57259724+pratt328@users.noreply.github.com>
2023-11-06 11:04:22 -0500

    react initialization!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="central-rectangle">
+          <p>
+            <code>Circuit Design For Kids!</code>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
